1c0fbe5c904bd9ea67f907c8fad0053f69216fcc
trixky <mathisbois.dev@gmail.com>
2020-06-11 19:00:15 +0200

    start build header

--- front/src/shared/components/Header.js
+++ front/src/shared/components/Header.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import './Header.css'
 
 class Header extends Component {
 	render() {
 		return (
 			<header>
-				<h1>matcha</h1>
+				<div className='header-top'>
+					<h1>matcha</h1>
+				</div>
+				<div className='header-bottom'>
+					<nav>
+						<h2>match me</h2>
+						<h2>search</h2>
+						<h2>visits</h2>
+						<h2>pretenders</h2>
+					</nav>
+				</div>
 			</header>
 		)
 	}
