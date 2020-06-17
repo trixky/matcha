8baf245df550bbc34b95930d31c8e0b40a60fa9e
cal <>
2020-06-17 14:31:44 +0200

    cal: added development tools

--- server/database/database.js
+++ server/database/database.js
@@ -1,16 +1,5 @@
 const pgp = require('pg-promise')();
 
-const database = pgp('postgres://user:password@localhost:5432/database');
-
-/*
-** Example
-** +) docker run --name database \
-                 -p 5432:5432 \
-                 -e POSTGRES_USER=user \
-                 -e POSTGRES_PASSWORD=password \
-                 -e POSTGRES_DB=database \
-                 -d \
-                 postgres
-*/
+const database = pgp('postgres://user:password@database:5432/database');
 
 module.exports = database;
--- server/database/seed.js
+++ server/database/seed.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
 
-const database = pgp('postgres://user:password@localhost:5432/database');
+const database = pgp('postgres://user:password@database:5432/database');
 
 function _exit(value){
     console.error(value);
