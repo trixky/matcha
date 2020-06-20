8f7a1ee0fe22a4f5384cb68566451a4c9d692a5a
pikooli <panamepoul@gmail.com>
2020-06-20 13:06:26 +0200

    add picture server in port 3002

---
+++ server/serverPicture.js
@@ -0,0 +1,53 @@
+const path = require("path")
+const http = require("http")
+var fs = require("fs")
+
+
+var dir = path.join(__dirname, "public")
+
+var mime = {
+    html: 'text/html',
+    txt: 'text/plain',
+    css: 'text/css',
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    svg: 'image/svg+xml',
+    js: 'application/javascript'
+};
+
+var server = http.createServer(function(req, res){
+    
+    var reqpath = req.url.toString().split('?')[0];
+    
+    if (req.method !== 'GET'){
+        res.statusCode = 501;
+        res.setHeader("Content-Type", "text/plain");
+        return res.end("Method not implemented");
+    }
+
+    var file = path.join(dir, reqpath)
+    
+    if (file.indexOf(dir, path.sep) !== 0){
+        res.statusCode = 403;
+        res.setHeader("Content-Type", "text/plain")
+        return res.end("Forbidden");
+    }
+    var type = mime[path.extname(file).slice(1)] || 'text/plain';
+
+    var s = fs.createReadStream(file);
+    s.on('open', function(){
+        res.setHeader("Content-Type", type);
+        s.pipe(res);
+    })
+
+    s.on("error", function(){
+        res.setHeader("Content-Type", "text/plain");
+        res.statusCode = 404;
+        return res.end("Not found")
+    })
+})
+
+server.listen(3002, function(){
+    console.log("Listening on port 3002")
+})
