61b9ba7dfd23ac73ed1570e8918d4bbd9ffb5c4f
trixky <mathisbois.dev@gmail.com>
2020-06-22 15:03:37 +0200

    change account states names

--- client/src/pages/account/Account.js
+++ client/src/pages/account/Account.js
@@ -6,8 +6,19 @@ import AccountOptions from './components/AccountOptions'
 
 class Account extends Component {
 	componentDidMount() {
+
 		if (this.props.readPage() !== 'Account')
 			this.props.setPage('Account');
+
+		const requestOptions = {
+			method: 'GET'
+		};
+		fetch('/account', requestOptions)
+			.then(response => response.json())
+			.then(data => {
+				console.log('attention data ! :')
+				console.log(data)
+			});
 	}
 
 	render() {
--- client/src/pages/account/components/States.js
+++ client/src/pages/account/components/States.js
@@ -12,25 +12,19 @@ class States extends Component {
 						<p className='state-type'>reputation</p><p className='state-number'>6676</p>
 					</li>
 					<li className='state-line'>
-						<p className='state-type state-link'><Link to='/'>match(s)</Link></p><p className='state-number'>21</p>
+						<p className='state-type state-link'><Link to='/'>matchs</Link></p><p className='state-number'>21</p>
 					</li>
 					<li className='state-line'>
-						<p className='state-type state-link'><Link to='/'>liker(s)</Link></p><p className='state-number'>42</p>
+						<p className='state-type state-link'><Link to='/'>likers</Link></p><p className='state-number'>42</p>
 					</li>
 					<li className='state-line'>
 						<p className='state-type state-link'><Link to='/'>liked</Link></p><p className='state-number'>37</p>
 					</li>
 					<li className='state-line'>
-						<p className='state-type state-link'><Link to='/'>viewer(s)</Link></p><p className='state-number'>421</p>
+						<p className='state-type state-link'><Link to='/'>viewers</Link></p><p className='state-number'>421</p>
 					</li>
 					<li className='state-line'>
-						<p className='state-type state-link'><Link to='/'>message(s)</Link></p><p className='state-number'>67</p>
-					</li>
-					<li className='state-line'>
-						<p className='state-type state-link'><Link to='/'>blocker(s)</Link></p><p className='state-number'>0</p>
-					</li>
-					<li className='state-line'>
-						<p className='state-type state-link'><Link to='/'>blocked</Link></p><p className='state-number'>1</p>
+						<p className='state-type state-link'><Link to='/'>blocked</Link></p><p className='state-number'>0</p>
 					</li>
 				</ul>
 			</div>
