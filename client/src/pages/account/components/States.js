3010676c0c4325c964df34311cd90c00312c0008
trixky <mathisbois.dev@gmail.com>
2020-06-27 17:40:31 +0200

    start fetch account finished + fix unmounted component

--- client/src/pages/account/components/States.js
+++ client/src/pages/account/components/States.js
@@ -4,27 +4,55 @@ import { Link } from "react-router-dom";
 import './States.css'
 
 class States extends Component {
+	_isMounted = false;
+
+	state = {
+		loading_message: 'loading...',
+		point_nbr: 0
+	}
+
+	componentDidMount() {
+		this._isMounted = true;
+		this.refresh_point_nbr();
+	}
+	
+	refresh_point_nbr() {
+		const _this = this;
+		
+		setTimeout(() => {
+			if (this._isMounted === true && !_this.state.data) {
+				const point_nbr = _this.state.point_nbr;
+				const new_point_nbr = (point_nbr === 3 ? 1 : point_nbr + 1);
+				_this.setState({ point_nbr: new_point_nbr, loading_message: 'loading' + '.'.repeat(new_point_nbr)})
+				_this.refresh_point_nbr();
+			}
+		}, 600);
+	}
+
+	componentWillUnmount() {
+		this._isMounted = false;
+	}
+
 	render() {
+		const data = this.props.data;
+
 		return (
 			<div className='states-containers'>
 				<ul>
 					<li className='state-line'>
-						<p className='state-type'>reputation</p><p className='state-number'>6676</p>
+						<p className='state-type'>reputation</p><p className='state-number'>{data ? data.reputation : this.state.loading_message}</p>
 					</li>
 					<li className='state-line'>
-						<p className='state-type state-link'><Link to='/'>matchs</Link></p><p className='state-number'>21</p>
+						<p className='state-type state-link'><Link to='/'>matchs</Link></p><p className='state-number'>{data ? data.match : this.state.loading_message}</p>
 					</li>
 					<li className='state-line'>
-						<p className='state-type state-link'><Link to='/'>likers</Link></p><p className='state-number'>42</p>
+						<p className='state-type state-link'><Link to='/'>likers</Link></p><p className='state-number'>{data ? data.likers : this.state.loading_message}</p>
 					</li>
 					<li className='state-line'>
-						<p className='state-type state-link'><Link to='/'>liked</Link></p><p className='state-number'>37</p>
+						<p className='state-type state-link'><Link to='/'>liked</Link></p><p className='state-number'>{data ? data.liked : this.state.loading_message}</p>
 					</li>
 					<li className='state-line'>
-						<p className='state-type state-link'><Link to='/'>viewers</Link></p><p className='state-number'>421</p>
-					</li>
-					<li className='state-line'>
-						<p className='state-type state-link'><Link to='/'>blocked</Link></p><p className='state-number'>0</p>
+						<p className='state-type state-link'><Link to='/'>viewers</Link></p><p className='state-number'>{data ? data.viewers : this.state.loading_message}</p>
 					</li>
 				</ul>
 			</div>
--- client/src/pages/chat/components/Pretender.js
+++ client/src/pages/chat/components/Pretender.js
@@ -6,7 +6,7 @@ import './Pretender.css'
 function Pretender() {
 	return (
 		<div className='pretender-chat-list'>
-			<img className='pretender-chat-list-img' src={Avatar} alt='Facebook logo' />
+			<img className='pretender-chat-list-img' src={Avatar} alt='profile image' />
 			<p className='pretender-chat-list-pseudo'>pseudo</p>
 		</div>
 	);
