161aa0d549f187e964f0a0191945a036f675bae8
trixky <mathisbois.dev@gmail.com>
2020-06-29 16:24:06 +0200

    finish update biography

--- client/src/pages/update_profile/components/UpdateBio.js
+++ client/src/pages/update_profile/components/UpdateBio.js
@@ -3,19 +3,61 @@ import React, { Component } from 'react'
 import './UpdateBio.css'
 
 class UpdateBio extends Component {
-	state = {
-		value: 'my bio here...'
+	constructor(props) {
+		super(props);
+		this.bio_fetched = false;
+		this.state = {
+			value: 'loading...'
+		}
+
+		this.componentDidUpdate = this.componentDidUpdate.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	componentDidUpdate() {
+		if (this.props.data && this.props.data.biography && !this.bio_fetched) {
+			this.bio_fetched = true;
+			this.setState({value: this.props.data.biography})
+		}
 	}
 
 	handleChange(event) {
 		this.setState({value: event.target.value})
 	}
 
+	handleClick(e) {
+		e.preventDefault();
+		
+		const body = {
+			user: {
+				biography: this.state.value
+			}
+		}
+
+		console.log(body)
+
+		const requestOptions = {
+			method: 'PUT',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(body)
+		};
+		fetch('/account/myprofile', requestOptions)
+			.then(response => response.json())
+			.then(data => {
+				console.log('cest bon')
+				console.log(data)
+		});
+	}
+
 	render() {
 		return (
 			<form>
 				<textarea className='form-input bio-input' type='textarea' placeholder='my bio here...' value={this.state.value} onChange={this.handleChange.bind(this)} />
-				<input className='form-input auth-submit' type='submit' value='update my bio' />
+				<input className='form-input auth-submit' type='submit' onClick={this.handleClick} value='update my bio' />
 			</form>
 		);
 	}
