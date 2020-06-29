bc7978bf4ab1b4d23fcc9ad053522a2d68ad2c7a
trixky <mathisbois.dev@gmail.com>
2020-06-29 20:29:31 +0200

    start update birthday

--- client/src/pages/update_profile/components/UpdateBirthday.js
+++ client/src/pages/update_profile/components/UpdateBirthday.js
@@ -3,19 +3,59 @@ import React, { Component } from 'react'
 import './UpdateBirthday.css'
 
 class UpdateBirthday extends Component {
-	state = {
-		value: '1999-12-13'
+	constructor(props) {
+		super(props);
+
+		this.birthday_fetched = false;
+
+		this.state = {
+			value: '2000-00-00'
+		}
+
+		this.componentDidUpdate = this.componentDidUpdate.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	componentDidUpdate() {
+		if (this.props.data && this.props.data.birthday && !this.birthday_fetched) {
+			this.birthday_fetched = true;
+			this.setState({ value: this.props.data.birthday })
+		}
+	}
+
+	handleClick(e) {
+		e.preventDefault();
+		
+		const body = {
+			user: {
+				birthday: this.state.value
+			}
+		}
+
+		console.log(body)
+
+		const requestOptions = {
+			method: 'PUT',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(body)
+		};
+		fetch('/account/myprofile', requestOptions)
 	}
 
 	handleChange(event) {
 		this.setState({value: event.target.value})
+		console.log(this.state.value)
 	}
 
 	render() {
 		return (
 			<form>
-				<input className='form-input' type='date' value={this.state.value} onChange={this.handleChange.bind(this)} />
-				<input className='form-input auth-submit' type='submit' value='update my birthday' />
+				<input className='form-input' type='date' value={this.state.value} onChange={this.handleChange} />
+				<input className='form-input auth-submit' type='submit' onClick={this.handleClick} value='update my birthday' />
 			</form>
 		);
 	}
