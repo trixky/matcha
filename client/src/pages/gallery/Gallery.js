c8ef3eaba6548920bbee65d7a9ef98fa7e55bc93
trixky <mathisbois.dev@gmail.com>
2020-07-02 16:08:31 +0200

    add gallery link in profile page

--- client/src/pages/gallery/Gallery.js
+++ client/src/pages/gallery/Gallery.js
@@ -5,9 +5,36 @@ import Left_row from '../../shared/img/interface_icone/previous.svg'
 import Right_row from '../../shared/img/interface_icone/next.svg'
 
 class Gallery extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: null,
+			selected: 1
+		}
+
+		this.componentDidMount = this.componentDidMount.bind(this);
+		this.handlePrevious = this.handlePrevious.bind(this);
+		this.handleNext = this.handleNext.bind(this);
+		this.picture_nbr = this.picture_nbr.bind(this);
+	}
+
 	componentDidMount() {
 		if (this.props.readPage() !== 'Gallery')
 			this.props.setPage('Gallery');
+
+		const current_user = window.location.pathname.split('/')[2];
+		console.log(current_user)
+
+		const requestOptions = {
+			method: 'GET',
+			// body
+		};
+		fetch('/account/' + current_user, requestOptions)
+			.then(response => response.json())
+			.then(data => {
+				console.log(data)
+				this.setState({ data: data._data })
+			});
 	}
 
 	handleNext() {
@@ -18,13 +45,25 @@ class Gallery extends Component {
 		console.log('previous image <-')
 	}
 
+	picture_nbr() {
+		if (this.state.data) {
+			for (let i = 1; i < 5; i++) {
+				if (this.state.data['picture' + i] === '') {
+					return (i);
+				}
+			}
+		}
+		return (-1)
+	}
+
 	render() {
+		const default_img = 'https://img.ohmymag.com/article/480/buzz/un-chat-bizarre-en-train-de-manger_31a53d5882ee4cb707c5dfaf5cf14895d0860a34.jpg';
 		return (
 			<div className='intern-page'>
 				<h2>gallery</h2>
-				<h3>1/4</h3>
+				<h3>{this.state.selected}/{this.picture_nbr() != -1 ? this.picture_nbr() : 'loading...'}</h3>
 				<div className='gallery-img-container' onClick={this.handleNext}>
-					<img className='gallery-img' src={'https://img.ohmymag.com/article/480/buzz/un-chat-bizarre-en-train-de-manger_31a53d5882ee4cb707c5dfaf5cf14895d0860a34.jpg'} alt='link to account page' />
+					<img className='gallery-img' src={this.state.data ? this.state.data.profile : default_img} alt='link to account page' />
 				</div>
 				<div className='gallery-row'>
 					<img className='header-img scale-hover' onClick={this.handlePrevious} src={Left_row} alt='link to account page' />
--- client/src/pages/profile/components/Images.js
+++ client/src/pages/profile/components/Images.js
@@ -4,15 +4,16 @@ import { withRouter } from 'react-router-dom';
 import './Images.css'
 
 class Images extends Component {
-	hangleClick(url) {
-		console.log('test')
-		this.props.history.push(url);
+	hangleClick(data) {
+		if (data) {
+			this.props.history.push('/profile/' + data.id + '/gallery');
+		}
 	}
 
 	render() {
 		const default_img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQH6-1GcLEZB6Z8LleMMZcWDkCincHaLOPlKA&usqp=CAU'
 		return (
-			<div onClick={() => this.hangleClick('/profile/username/gallery')} className='profil-img-container'>
+			<div onClick={() => this.hangleClick(this.props.data)} className='profil-img-container'>
 				<img className='cercle-img profile-img' src={this.props.data ? this.props.data.profile : default_img} alt='profile' />
 				<div className='cercle-img cercle-img-1'></div>
 				<div className='cercle-img cercle-img-2'></div>
