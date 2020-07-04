99295d7d7dde185976999d83728c8e640e35462f
trixky <mathisbois.dev@gmail.com>
2020-07-04 15:40:35 +0200

    fetch notifications on notification page

--- client/src/pages/notification/Notification.js
+++ client/src/pages/notification/Notification.js
@@ -5,25 +5,43 @@ import './Notification.css'
 import NotificationLine from './components/NotificationLine'
 
 class Notification extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			notifications: []
+		}
+		this.componentDidMount = this.componentDidMount.bind(this);
+		this.fetchNotification = this.fetchNotification.bind(this);
+	}
+
 	componentDidMount() {
 		this.date = Date.now();
 
 		if (this.props.readPage() !== 'Notification')
 			this.props.setPage('Notification');
+		this.fetchNotification()
 	}
+
+	fetchNotification() {
+		const requestOptions = {
+			method: 'GET',
+		};
+		fetch('/notifications', requestOptions)
+			.then(response => response.json())
+			.then(data => {
+				console.log(data)
+				this.setState({ notifications: data._data})
+			});
+	}
+
+
 
 	render() {
 		return (
 			<div className='intern-page'>
 				<div className='notification-container'>
-					<NotificationLine type='liked' username='toto' date={this.date} />
-					<NotificationLine type='liked back' username='toto' date={this.date} />
-					<NotificationLine type='unliked' username='toto' date={this.date} />
-					<NotificationLine type='blocked' username='toto' date={this.date} />
-					<NotificationLine type='unblocked' date={this.date} />
-					<NotificationLine type='seen' username='toto' date={this.date} />
+					{this.state.notifications.map((info, index) => <NotificationLine key={index + Date.now()} info={info} />)}
 				</div>
-				<input className='form-input notification-more-input' type='submit' value='show more notifications' />
 			</div>
 		);
 	}
--- client/src/pages/notification/components/NotificationLine.js
+++ client/src/pages/notification/components/NotificationLine.js
@@ -3,46 +3,11 @@ import React, { Component } from 'react'
 import './NotificationLine.css'
 
 class NotificationLine extends Component {
-	time_generator(date) {
-		console.log(date)
-		return `${Date.now() + 5 - date} sec`
-	}
-
 	render() {
-		this.emoji = '⚠️'
-		this.message = 'loading error'
-		switch (this.props.type) {
-			case 'liked':
-				this.emoji = '❤️'
-				this.message = 'like you'
-				break;
-			case 'liked back':
-				this.emoji = '💕'
-				this.message = 'liked you back'
-				break;
-			case 'unliked':
-				this.emoji = '💔'
-				this.message = 'doesn\'t love you anymore'
-				break;
-			case 'blocked':
-				this.emoji = '🤐'
-				this.message = 'blocked you'
-				break;
-			case 'unblocked':
-				this.emoji = '😇'
-				this.message = 'unblocked you'
-				break;
-				case 'seen':
-				this.emoji = '👁️'
-				this.message = 'saw your profile'
-				break;
-			default:
-				break;
-
-		}
+		const info = this.props.info
 		return (
 			<div className='notification-line'>
-				<p>{this.emoji} {this.props.username != null ? this.props.username : 'someone'} {this.message} <span className='notification-time'>{this.time_generator(this.props.date)}</span></p>
+				<p>{info.notification} <span className='notification-time'>{info.created}</span></p>
 			</div>
 		);
 	}
