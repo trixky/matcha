6b15f0c757605d6e8a86654231fa580d5e56ddb5
trixky <mathisbois.dev@gmail.com>
2020-07-05 12:18:28 +0200

    finish get post message on chat page

---
+++ client/src/pages/chat/components/Message.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import './Message.css'
+
+class Message extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		const info = this.props.info;
+		return (
+			<div className={'message ' + (this.props.pretender === info.sender ? 'message-left' : 'message-right')}>
+				<p className='message-time'>{info.created}</p>
+				<p>{info.message}</p>
+			</div>
+		);
+	}
+}
+
+export default Message;
