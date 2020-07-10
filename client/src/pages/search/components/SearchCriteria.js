d84c38dbce815e4051f747f5d15ea0e86eac1e77
trixky <mathisbois.dev@gmail.com>
2020-07-10 13:38:54 +0200

    add age and reputation to search filter (search filter finished)"

--- client/src/pages/search/components/SearchCriteria.js
+++ client/src/pages/search/components/SearchCriteria.js
@@ -8,6 +8,8 @@ class SearchCriteria extends Component {
 
 		this.handleGender = this.handleGender.bind(this);
 		this.handleDistance = this.handleDistance.bind(this);
+		this.handleAge = this.handleAge.bind(this);
+		this.handleReputation = this.handleReputation.bind(this);
 	}
 
 	handleGender(event) {
@@ -16,7 +18,7 @@ class SearchCriteria extends Component {
 		if (value === 'select a gender') {
 			this.props.parent.gender = undefined
 		} else {
-			this.props.parent.gender = event.currentTarget.value
+			this.props.parent.gender = value
 		}
 		this.props.parent.refresh_users()
 	}
@@ -27,7 +29,31 @@ class SearchCriteria extends Component {
 		if (value === 'select a distance') {
 			this.props.parent.distanceMax = undefined
 		} else {
-			this.props.parent.distanceMax = parseInt(event.currentTarget.value, 10)
+			this.props.parent.distanceMax = parseInt(value, 10)
+		}
+		this.props.parent.refresh_users()
+	}
+	
+	handleReputation(event) {
+		const value = event.currentTarget.value;
+		
+		if (value === 'select a reputation') {
+			this.props.parent.repuMin = undefined
+		} else {
+			this.props.parent.repuMin = parseInt(value, 10)
+		}
+		this.props.parent.refresh_users()
+	}
+
+	handleAge(event) {
+		const value = event.currentTarget.value;
+		
+		if (value === 'select an age slice') {
+			this.props.parent.ageMin = undefined
+			this.props.parent.ageMax = undefined
+		} else {
+			this.props.parent.ageMin = parseInt(value.slice(0, 2), 10)
+			this.props.parent.ageMax = parseInt(value.slice(5, 8), 10)
 		}
 		this.props.parent.refresh_users()
 	}
@@ -41,13 +67,7 @@ class SearchCriteria extends Component {
 					<option value="women">women</option>
 					<option value="non binary">non binary</option>
 				</select>
-				<select className='form-input' name="orientation" id="orientation">
-					<option value="select an orientation">select an orientation</option>
-					<option value="heterosexual">heterosexual</option>
-					<option value="homosexual">homosexual</option>
-					<option value="bisexual">bisexual</option>
-				</select>
-				<select className='form-input' name="orientation" id="orientation">
+				<select onChange={this.handleAge} className='form-input' name="age" id="age">
 					<option value="select an age slice">select an age slice</option>
 					<option value="18 - 25">18 - 25</option>
 					<option value="20 - 25">20 - 25</option>
@@ -55,9 +75,19 @@ class SearchCriteria extends Component {
 					<option value="30 - 35">30 - 35</option>
 					<option value="35 - 40">35 - 40</option>
 					<option value="40 - 50">40 - 50</option>
-					<option value="+ 50">+ 50</option>
+					<option value="50 - 99">40 - 50</option>
 				</select>
-				<select onChange={this.handleDistance} className='form-input' name="orientation" id="orientation">
+				<select onChange={this.handleReputation} className='form-input' name="reputation" id="reputation">
+					<option value="select a reputation">select a reputation</option>
+					<option value="50">+ 50</option>
+					<option value="100">+ 100</option>
+					<option value="200">+ 200</option>
+					<option value="500">+ 500</option>
+					<option value="1000">+ 1000</option>
+					<option value="3000">+ 3000</option>
+					<option value="10000">+ 10000</option>
+				</select>
+				<select onChange={this.handleDistance} className='form-input' name="distance" id="distance">
 					<option value="select a distance">select a distance</option>
 					<option value="25">25 km</option>
 					<option value="50">50 km</option>
